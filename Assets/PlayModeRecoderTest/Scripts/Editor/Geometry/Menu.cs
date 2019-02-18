6c638c7571870710ca26c32b414e251511e88a5c
MizoTake <mizotake123@gmail.com>
2019-02-19 02:23:07 +0900

    add menu

---
+++ Assets/PlayModeRecoderTest/Scripts/Editor/Geometry/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayModeRecoderTest
+{
+    public class Menu : IDrawable, IChoosable
+    {
+        public void Draw ()
+        {
+
+        }
+
+        public void Choice ()
+        {
+
+        }
+
+        public GUIContent[] SelectableItem ()
+        {
+            return new GUIContent[0];
+        }
+    }
+}
